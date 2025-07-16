4c1f8d9cdad290a1bf54f01df21a2e96f111db1c
Mikaelarth <140899435+Mikaelarth@users.noreply.github.com>
2025-07-16 15:23:18 +0000

    Add Qt Test suite

---
+++ tests/database_test.cpp
@@ -0,0 +1,58 @@
+#include <QtTest>
+#include <QCoreApplication>
+#include "DatabaseManager.h"
+#include <QSqlQuery>
+#include <QSqlRecord>
+
+class DatabaseManagerTest : public QObject
+{
+    Q_OBJECT
+private slots:
+    void connectionFailure();
+};
+
+void DatabaseManagerTest::connectionFailure()
+{
+    DatabaseManager db;
+    QVERIFY(!db.open());
+    QVERIFY(!db.lastError().isEmpty());
+    db.close();
+}
+
+class SQLiteQueryTest : public QObject
+{
+    Q_OBJECT
+private slots:
+    void simpleSelect();
+};
+
+void SQLiteQueryTest::simpleSelect()
+{
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE", "memdb");
+    db.setDatabaseName(":memory:");
+    QVERIFY(db.open());
+
+    QSqlQuery query(db);
+    QVERIFY(query.exec("CREATE TABLE test(id INTEGER PRIMARY KEY, name TEXT);"));
+    QVERIFY(query.exec("INSERT INTO test(name) VALUES('Alice');"));
+    QVERIFY(query.exec("SELECT name FROM test WHERE id=1;"));
+    QVERIFY(query.next());
+    QCOMPARE(query.value(0).toString(), QString("Alice"));
+
+    db.close();
+    QSqlDatabase::removeDatabase("memdb");
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication app(argc, argv);
+    int status = 0;
+    DatabaseManagerTest dbTest;
+    status |= QTest::qExec(&dbTest, argc, argv);
+    SQLiteQueryTest queryTest;
+    status |= QTest::qExec(&queryTest, argc, argv);
+    return status;
+}
+
+#include "database_test.moc"
+
