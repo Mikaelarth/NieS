758c6dc41b333f58564a26e5574c06dc57f2a223
Mikaelarth <140899435+Mikaelarth@users.noreply.github.com>
2025-07-18 23:15:56 +0000

    Add ProductManager with CRUD operations

---
+++ src/ProductManager.h
@@ -0,0 +1,25 @@
+#ifndef PRODUCTMANAGER_H
+#define PRODUCTMANAGER_H
+
+#include <QObject>
+#include <QList>
+#include <QVariantMap>
+
+class ProductManager : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ProductManager(QObject *parent = nullptr);
+
+    bool addProduct(const QString &name, double price, double discount = 0.0);
+    bool updateProduct(int id, const QString &name, double price, double discount);
+    bool deleteProduct(int id);
+    QList<QVariantMap> listProducts();
+
+    QString lastError() const;
+
+private:
+    QString m_lastError;
+};
+
+#endif // PRODUCTMANAGER_H
