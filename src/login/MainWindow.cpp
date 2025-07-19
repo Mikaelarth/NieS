427e57f48416852102f7fa258cd6974a49d4f337
Mikaelarth <140899435+Mikaelarth@users.noreply.github.com>
2025-07-19 17:53:37 +0000

    Add ProductWindow UI and tests

--- src/login/MainWindow.cpp
+++ src/login/MainWindow.cpp
@@ -1,8 +1,26 @@
 #include "MainWindow.h"
+#include "products/ProductWindow.h"
+#include "ProductManager.h"
+#include <QMenuBar>
+#include <QMenu>
+#include <QAction>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     setWindowTitle(tr("NieS"));
+
+    QMenu *prodMenu = menuBar()->addMenu(tr("Products"));
+    QAction *manageAct = prodMenu->addAction(tr("Manage Products"));
+    connect(manageAct, &QAction::triggered, this, &MainWindow::openProducts);
+}
+
+void MainWindow::openProducts()
+{
+    if (!m_productWindow)
+        m_productWindow = new ProductWindow(new ProductManager(this), this);
+    m_productWindow->show();
+    m_productWindow->raise();
+    m_productWindow->activateWindow();
 }
 
---
+++ src/products/ProductWindow.h
@@ -0,0 +1,37 @@
+#ifndef PRODUCTWINDOW_H
+#define PRODUCTWINDOW_H
+
+#include <QWidget>
+
+class QTableWidget;
+class QLineEdit;
+class QDoubleSpinBox;
+class QPushButton;
+class ProductManager;
+
+class ProductWindow : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit ProductWindow(ProductManager *pm, QWidget *parent = nullptr);
+
+public slots:
+    void loadProducts();
+
+private slots:
+    void onAdd();
+    void onEdit();
+    void onDelete();
+
+private:
+    ProductManager *m_pm;
+    QTableWidget *m_table;
+    QLineEdit *m_nameEdit;
+    QDoubleSpinBox *m_priceEdit;
+    QDoubleSpinBox *m_discountEdit;
+    QPushButton *m_addBtn;
+    QPushButton *m_editBtn;
+    QPushButton *m_deleteBtn;
+};
+
+#endif // PRODUCTWINDOW_H
