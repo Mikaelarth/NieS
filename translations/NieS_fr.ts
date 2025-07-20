5313d6574cb6fe643bb9cae25e89723fcc9cf469
Mikaelarth <140899435+Mikaelarth@users.noreply.github.com>
2025-07-20 00:23:26 +0000

    Add French translations and document new language steps

--- translations/NieS_fr.ts
+++ translations/NieS_fr.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="src/dashboard/DashboardWindow.cpp" line="10"/>
         <source>Dashboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Tableau de bord</translation>
     </message>
     <message>
         <location filename="src/dashboard/DashboardWindow.cpp" line="12"/>
         <source>Sales and inventory KPIs will be shown here.</source>
-        <translation type="unfinished"></translation>
+        <translation>Les indicateurs de vente et de stock seront affichés ici.</translation>
     </message>
 </context>
 <context>
@@ -20,22 +20,22 @@
         <location filename="src/login/LoginDialog.cpp" line="14"/>
         <location filename="src/login/LoginDialog.cpp" line="20"/>
         <source>Login</source>
-        <translation type="unfinished"></translation>
+        <translation>Connexion</translation>
     </message>
     <message>
         <location filename="src/login/LoginDialog.cpp" line="24"/>
         <source>Username</source>
-        <translation type="unfinished"></translation>
+        <translation>Nom d'utilisateur</translation>
     </message>
     <message>
         <location filename="src/login/LoginDialog.cpp" line="25"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Mot de passe</translation>
     </message>
     <message>
         <location filename="src/login/LoginDialog.cpp" line="46"/>
         <source>Login failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Échec de la connexion</translation>
     </message>
 </context>
 <context>
@@ -43,32 +43,32 @@
     <message>
         <location filename="src/login/MainWindow.cpp" line="16"/>
         <source>NieS</source>
-        <translation type="unfinished"></translation>
+        <translation>NieS</translation>
     </message>
     <message>
         <location filename="src/login/MainWindow.cpp" line="18"/>
         <source>Products</source>
-        <translation type="unfinished"></translation>
+        <translation>Produits</translation>
     </message>
     <message>
         <location filename="src/login/MainWindow.cpp" line="19"/>
         <source>Manage Products</source>
-        <translation type="unfinished"></translation>
+        <translation>Gérer les produits</translation>
     </message>
     <message>
         <location filename="src/login/MainWindow.cpp" line="23"/>
         <source>Sales</source>
-        <translation type="unfinished"></translation>
+        <translation>Ventes</translation>
     </message>
     <message>
         <location filename="src/login/MainWindow.cpp" line="24"/>
         <source>Point of Sale</source>
-        <translation type="unfinished"></translation>
+        <translation>Point de vente</translation>
     </message>
     <message>
         <location filename="src/login/MainWindow.cpp" line="28"/>
         <source>Sales Report</source>
-        <translation type="unfinished"></translation>
+        <translation>Rapport des ventes</translation>
     </message>
 </context>
 <context>
@@ -77,75 +77,75 @@
         <location filename="src/sales/POSWindow.cpp" line="20"/>
         <location filename="src/sales/POSWindow.cpp" line="95"/>
         <source>Return</source>
-        <translation type="unfinished"></translation>
+        <translation>Retour</translation>
     </message>
     <message>
         <location filename="src/sales/POSWindow.cpp" line="21"/>
         <source>Print Invoice</source>
-        <translation type="unfinished"></translation>
+        <translation>Imprimer la facture</translation>
     </message>
     <message>
         <location filename="src/sales/POSWindow.cpp" line="26"/>
         <source>Point of Sale</source>
-        <translation type="unfinished"></translation>
+        <translation>Point de vente</translation>
     </message>
     <message>
         <location filename="src/sales/POSWindow.cpp" line="36"/>
         <source>Sell</source>
-        <translation type="unfinished"></translation>
+        <translation>Vendre</translation>
     </message>
     <message>
         <location filename="src/sales/POSWindow.cpp" line="40"/>
         <source>Cash</source>
-        <translation type="unfinished"></translation>
+        <translation>Espèces</translation>
     </message>
     <message>
         <location filename="src/sales/POSWindow.cpp" line="40"/>
         <location filename="src/sales/POSWindow.cpp" line="83"/>
         <source>Card</source>
-        <translation type="unfinished"></translation>
+        <translation>Carte</translation>
     </message>
     <message>
         <location filename="src/sales/POSWindow.cpp" line="40"/>
         <location filename="src/sales/POSWindow.cpp" line="85"/>
         <source>Mobile Money</source>
-        <translation type="unfinished"></translation>
+        <translation>Mobile money</translation>
     </message>
     <message>
         <location filename="src/sales/POSWindow.cpp" line="40"/>
         <location filename="src/sales/POSWindow.cpp" line="87"/>
         <source>QR Code</source>
-        <translation type="unfinished"></translation>
+        <translation>Code QR</translation>
     </message>
     <message>
         <location filename="src/sales/POSWindow.cpp" line="46"/>
         <source>Product</source>
-        <translation type="unfinished"></translation>
+        <translation>Produit</translation>
     </message>
     <message>
         <location filename="src/sales/POSWindow.cpp" line="47"/>
         <source>Quantity</source>
-        <translation type="unfinished"></translation>
+        <translation>Quantité</translation>
     </message>
     <message>
         <location filename="src/sales/POSWindow.cpp" line="78"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Erreur</translation>
     </message>
     <message>
         <location filename="src/sales/POSWindow.cpp" line="95"/>
         <source>Return processed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Retour traité.</translation>
     </message>
     <message>
         <location filename="src/sales/POSWindow.cpp" line="100"/>
         <source>Invoice</source>
-        <translation type="unfinished"></translation>
+        <translation>Facture</translation>
     </message>
     <message>
         <location filename="src/sales/POSWindow.cpp" line="100"/>
         <source>Invoice printed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Facture imprimée.</translation>
     </message>
 </context>
 <context>
@@ -153,52 +153,52 @@
     <message>
         <location filename="src/products/ProductWindow.cpp" line="17"/>
         <source>Products</source>
-        <translation type="unfinished"></translation>
+        <translation>Produits</translation>
     </message>
     <message>
         <location filename="src/products/ProductWindow.cpp" line="22"/>
         <source>ID</source>
-        <translation type="unfinished"></translation>
+        <translation>ID</translation>
     </message>
     <message>
         <location filename="src/products/ProductWindow.cpp" line="22"/>
         <location filename="src/products/ProductWindow.cpp" line="46"/>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Nom</translation>
     </message>
     <message>
         <location filename="src/products/ProductWindow.cpp" line="22"/>
         <location filename="src/products/ProductWindow.cpp" line="47"/>
         <source>Price</source>
-        <translation type="unfinished"></translation>
+        <translation>Prix</translation>
     </message>
     <message>
         <location filename="src/products/ProductWindow.cpp" line="22"/>
         <location filename="src/products/ProductWindow.cpp" line="48"/>
         <source>Discount</source>
-        <translation type="unfinished"></translation>
+        <translation>Remise</translation>
     </message>
     <message>
         <location filename="src/products/ProductWindow.cpp" line="37"/>
         <source>Add</source>
-        <translation type="unfinished"></translation>
+        <translation>Ajouter</translation>
     </message>
     <message>
         <location filename="src/products/ProductWindow.cpp" line="38"/>
         <source>Edit</source>
-        <translation type="unfinished"></translation>
+        <translation>Modifier</translation>
     </message>
     <message>
         <location filename="src/products/ProductWindow.cpp" line="39"/>
         <source>Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>Supprimer</translation>
     </message>
     <message>
         <location filename="src/products/ProductWindow.cpp" line="84"/>
         <location filename="src/products/ProductWindow.cpp" line="100"/>
         <location filename="src/products/ProductWindow.cpp" line="113"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Erreur</translation>
     </message>
 </context>
 <context>
@@ -206,32 +206,32 @@
     <message>
         <location filename="src/sales/SalesReportWindow.cpp" line="12"/>
         <source>Sales Report</source>
-        <translation type="unfinished"></translation>
+        <translation>Rapport des ventes</translation>
     </message>
     <message>
         <location filename="src/sales/SalesReportWindow.cpp" line="17"/>
         <source>ID</source>
-        <translation type="unfinished"></translation>
+        <translation>ID</translation>
     </message>
     <message>
         <location filename="src/sales/SalesReportWindow.cpp" line="17"/>
         <source>Product ID</source>
-        <translation type="unfinished"></translation>
+        <translation>ID produit</translation>
     </message>
     <message>
         <location filename="src/sales/SalesReportWindow.cpp" line="17"/>
         <source>Quantity</source>
-        <translation type="unfinished"></translation>
+        <translation>Quantité</translation>
     </message>
     <message>
         <location filename="src/sales/SalesReportWindow.cpp" line="17"/>
         <source>Date</source>
-        <translation type="unfinished"></translation>
+        <translation>Date</translation>
     </message>
     <message>
         <location filename="src/sales/SalesReportWindow.cpp" line="17"/>
         <source>Total</source>
-        <translation type="unfinished"></translation>
+        <translation>Total</translation>
     </message>
 </context>
 </TS>
